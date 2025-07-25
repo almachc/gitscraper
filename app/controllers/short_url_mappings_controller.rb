48ad96080333fa9ed077a3e0d6f5dfa01b1c91e1
Alexandre Cruz <alexandre.machcruz@gmail.com>
2025-07-25 12:48:29 -0300

    fix: improve external redirect security and resolve brakeman warning

--- app/controllers/short_url_mappings_controller.rb
+++ app/controllers/short_url_mappings_controller.rb
@@ -1,8 +1,19 @@
 class ShortUrlMappingsController < ApplicationController
+  ALLOWED_HOSTS = [ 'github.com' ]
+
   def redirect
     short_url_mapping = ShortUrlMapping.find_by!(code: params[:code])
-    redirect_to short_url_mapping.original_url, allow_other_host: true
+    uri = URI.parse(short_url_mapping.original_url)
+
+    unless uri.is_a?(URI::HTTPS) && ALLOWED_HOSTS.include?(uri.host)
+      head :unprocessable_entity
+      return
+    end
+
+    redirect_to uri.to_s, allow_other_host: true
   rescue ActiveRecord::RecordNotFound
     render plain: t('.url_not_found'), status: :not_found
+  rescue URI::InvalidURIError
+    head :unprocessable_entity
   end
 end
